6dd5e7e1ba06bd46eb06b6f16fffd70d1a707aca
glotovdi <glotovdi@gmail.com>
2018-02-17 16:12:06 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hola, " + person;
+}
+var krunal = greeter('Senossskkkkkta');
+console.log(krunal);
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+var start = document.getElementById("start");               
+var textnode = document.createTextNode("Let's start");                            
+start.appendChild(textnode); 
